cd14236b63257c8605fabdbe185a6f1e8917ae53
Kayode Obayomi <obayomikayode2015@gmil.com>
2025-05-13 17:26:14 +0100

    Responsive

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu_bar");
+const closeBtn = document.querySelector("#close_btn");
+
+
+const themeToggler = document.querySelector(".theme.toggler");
+
+menuBtn.addEventListener('click',()=>{
+    sideMenu.style.display = "block"
+}) 
+closeBtn.addEventListener('click',()=>{
+    sideMenu.style.display = "none"
+}) 
+
+themeToggler.addEventListener('click', ()=>
+{
+    document.body.classList.toggle('dark-theme-variables');
+
+    const firstSpan = themeToggler.querySelector('span:nth-child(1)');
+    const secondSpan = themeToggler.querySelector('span:nth-child(2)');
+    firstSpan.classList.toggle('active');
+    secondSpan.classList.toggle('active');
+});
+
+ 
